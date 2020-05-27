e09a67edd10e751e5840c69db3846193cbffcd93
sk82jack <sk82jack@users.noreply.github.com>
2020-05-27 21:21:12 +0100

    add code to add data to datastore

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from google.cloud import datastore
 
 def scrap_gt86_checker(data, context):
     result = requests.get('http://www.dewsburyautosalvage.com/catalogsearch/result/index/?limit=all&q=gt86&x=0&y=0')
     soup = BeautifulSoup(result.content, 'lxml')
 
-    number = int(soup.find("p", {"class": "amount"}).strong.text.split()[0])
+    client = datastore.Client()
+    kind = 'gt86-scrap-checker'
 
     for products_grid in soup.find_all('ul', {'class': 'products-grid'}):
         for item in products_grid.find_all('li'):
             car = item.h2.a
             # print(car.attrs['title'])
             # print (car.attrs['href'])
+
+            entity = datastore.Entity(client.key(kind, car.attrs['href']))
+            entity.update({
+                'title': car.attrs['title'],
+                'URL': car.attrs['href'],
+            })
+            client.put(entity)
